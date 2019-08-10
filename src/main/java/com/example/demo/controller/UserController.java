a2c0f98e5f889d4433fc9adf7ae83fe987a5a38a
wangqiming1001 <52316484+wangqiming1001@users.noreply.github.com>
2019-08-10 16:34:39 +0800

    Update UserController.java

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -20,18 +20,21 @@ public class UserController {
 
     @GetMapping("/user/{userId}")
     public User findById(@PathVariable long userId) {
-        return null;
+        return userMapper.findById(userId);
     }
 
     @PostMapping("/user")
     public void insertUser(@RequestBody User user) {
+        userMapper.insertUser(user);
     }
 
     @PutMapping("/user/{userId}")
     public void updateUser(@RequestBody User user,@PathVariable int userId) {
+        userMapper.updateUser(userId, user.getName());
     }
 
     @DeleteMapping("/user/{userId}")
     public void deleteById(@PathVariable long userId) {
+        userMapper.deleteUser(userId);
     }
 }
